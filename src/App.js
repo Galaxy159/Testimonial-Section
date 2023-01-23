db3ac15dc39a37356978b45832d25ee6b9171742
Galaxy159 <braun.asaf@gmail.com>
2023-01-23 18:06:17 +0200

    show footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import "./style/App.css";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
     <>
       <main className="container">
-        <h1>some healine</h1>
         <Testimonials />
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
