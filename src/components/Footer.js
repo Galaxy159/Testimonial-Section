1781095dd16af6044d62baf09bf39597553bd428
Galaxy159 <braun.asaf@gmail.com>
2023-01-23 15:10:35 +0200

    mobile

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href="https://www.frontendmentor.io/profile/Galaxy159"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Galaxy159
+        </a>
+        .
+      </div>
+    </>
+  );
+}
